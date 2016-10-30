9d9e6192562ba306baace60dea92e4957a4f7715
Jacopo Mangiavacchi <jacopo.mangiavacchi@uk.ibm.com>
2016-10-30 15:12:10 +0000

    updated travis script to swift version 3.0 release

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -5,8 +5,8 @@ set -ev
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
 	DIR="$(pwd)"
 	cd ..
-	export SWIFT_VERSION=swift-DEVELOPMENT-SNAPSHOT-2016-05-09-a
-	wget https://swift.org/builds/development/ubuntu1404/${SWIFT_VERSION}/${SWIFT_VERSION}-ubuntu14.04.tar.gz
+	export SWIFT_VERSION=swift-3.0-RELEASE
+	wget https://swift.org/builds/swift-3.0-release/ubuntu1404/${SWIFT_VERSION}/${SWIFT_VERSION}-ubuntu14.04.tar.gz
 	tar xzf $SWIFT_VERSION-ubuntu14.04.tar.gz
 	export PATH="${PWD}/${SWIFT_VERSION}-ubuntu14.04/usr/bin:${PATH}"
 	cd "$DIR"
