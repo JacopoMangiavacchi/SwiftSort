97f0708a534ec1eb1e805a7ec22b9564fc437943
Jacopo Mangiavacchi <jacopo.mangiavacchi@uk.ibm.com>
2016-11-01 17:36:33 +0000

    Update SwiftSort.swift

--- Sources/SwiftSort.swift
+++ Sources/SwiftSort.swift
@@ -86,77 +86,79 @@ extension Array where Element : Comparable {
     
     //nlogn
     mutating func quickSort()  {
-        quickSort(low: 0, high: self.count-1)
-    }
-    internal mutating func quickSort(low: Int, high: Int) {
-        if high-low > 0 {
-            let pivot = partition(low: low, high: high)
-            quickSort(low: low, high: pivot-1)
-            quickSort(low: pivot+1, high: high)
-        }
-    }
-    internal mutating func partition(low: Int, high: Int) -> Int {
-        //var pivot = high
-        var firstHight = low
-        for i in low..<high {
-            if self[i] < self[high] {
-                swap(i, firstHight)
-                firstHight += 1
+        func quickSort(low: Int, high: Int) {
+            if high-low > 0 {
+                let pivot = partition(low: low, high: high)
+                quickSort(low: low, high: pivot-1)
+                quickSort(low: pivot+1, high: high)
             }
         }
-        swap(high, firstHight)
-        return firstHight
+        func partition(low: Int, high: Int) -> Int {
+            //var pivot = high
+            var firstHight = low
+            for i in low..<high {
+                if self[i] < self[high] {
+                    swap(i, firstHight)
+                    firstHight += 1
+                }
+            }
+            swap(high, firstHight)
+            return firstHight
+        }
+
+        quickSort(low: 0, high: self.count-1)
     }
-    
+
     
     //nlogn
     mutating func mergeSort()  {
+        func mergeSort(low: Int, high: Int) {
+            let middle = (low+high)/2
+            
+            if high-low > 1 {
+                mergeSort(low: low, high: middle)
+                mergeSort(low: middle+1, high: high)
+            }
+            merge(low: low, middle: middle, high: high)
+        }
+        func merge(low: Int, middle: Int, high: Int) {
+            guard low != middle || middle != high else {
+                return
+            }
+            
+            var leftQ = Queue<Element>()
+            var rightQ = Queue<Element>()
+            
+            for i in low...middle {
+                leftQ.enqueue(self[i])
+            }
+            for i in middle+1...high {
+                rightQ.enqueue(self[i])
+            }
+            
+            var i = low
+            while !leftQ.isEmpty() && !rightQ.isEmpty() {
+                if leftQ.head()! < rightQ.head()! {
+                    self[i] = leftQ.dequeue()!
+                }
+                else {
+                    self[i] = rightQ.dequeue()!
+                }
+                i += 1
+            }
+            while let left =  leftQ.dequeue() {
+                self[i] = left
+                i += 1
+            }
+            while let right =  rightQ.dequeue() {
+                self[i] = right
+                i += 1
+            }
+        }
+    
         mergeSort(low: 0, high: self.count-1)
     }
-    internal mutating func mergeSort(low: Int, high: Int) {
-        let middle = (low+high)/2
-        
-        if high-low > 1 {
-            mergeSort(low: low, high: middle)
-            mergeSort(low: middle+1, high: high)
-        }
-        merge(low: low, middle: middle, high: high)
-    }
-    internal mutating func merge(low: Int, middle: Int, high: Int) {
-        guard low != middle || middle != high else {
-            return
-        }
-        
-        var leftQ = Queue<Element>()
-        var rightQ = Queue<Element>()
-        
-        for i in low...middle {
-            leftQ.enqueue(self[i])
-        }
-        for i in middle+1...high {
-            rightQ.enqueue(self[i])
-        }
-        
-        var i = low
-        while !leftQ.isEmpty() && !rightQ.isEmpty() {
-            if leftQ.head()! < rightQ.head()! {
-                self[i] = leftQ.dequeue()!
-            }
-            else {
-                self[i] = rightQ.dequeue()!
-            }
-            i += 1
-        }
-        while let left =  leftQ.dequeue() {
-            self[i] = left
-            i += 1
-        }
-        while let right =  rightQ.dequeue() {
-            self[i] = right
-            i += 1
-        }
-    }
-    
+
     //nlogn
     mutating func heapSort() {
         var heap = PriorityQueue<Element>(fromArray: self)
